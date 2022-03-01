862262ac86c50dfc47f377720af509391c45f837
Luc Van Minh <minh.lucvan@ncc.asia>
2022-03-01 07:27:14 +0700

    Merged PR 15456: update env

--- run-judge.sh
+++ run-judge.sh
@@ -3,10 +3,10 @@
 docker run \
 --name judge \
 --network host \
--v /home/nccsoft/dmoj-docker/dmoj/problems:/problems \
+-v $(cd ../../ ; pwd)/dmoj/problems:/problems \
 --cap-add=SYS_PTRACE \
 -d \
 --restart=always \
 dmoj/judge-tier2:latest \
-run -p "9999" -c /problems/judge.yml \
+run -p "9999" -e="V8JS" -c /problems/judge.yml \
 "127.0.0.1" "ncc-tier2" "4fg/NMnIHi5NWGILMnAi7Qt7iSVTnDednfBx2A5GRbDEyTiH/+vHFKqg+taNLNb53VYbsj+Cno0cG8xi/7LqL3VKUhqyiOPpXDyN"
